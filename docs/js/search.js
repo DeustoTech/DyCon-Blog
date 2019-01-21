56ac0cdd7f7fadef6ad4cd5187b5a15abfedf0dc
Jesus Oroya <djoroya@deusto.es>
2019-01-21 10:02:43 +0100

    Repair url

--- docs/js/search.js
+++ docs/js/search.js
@@ -9,7 +9,7 @@ jQuery(function() {
   });
 
   // Get the generated search_data.json file so lunr.js can search it locally.
-  window.data = $.getJSON('/DyConBlog/search/search_data.json');
+  window.data = $.getJSON('/DyCon-Blog/search/search_data.json');
 
   // Wait for the data to load and add it to lunr
   window.data.then(function(loaded_data){
